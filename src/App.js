b6334dfbb16677f7841002a0434d75fbc7705c0e
danish <dannush786@gmail.com>
2021-05-02 21:20:08 +0530

    completed for now

--- src/App.js
+++ src/App.js
@@ -3,19 +3,22 @@ import Login from "./components/Login";
 import Header from "./components/Header";
 import './App.css';
 import Home from "./components/Home";
+import Detail from "./components/Detail";
 
 function App() {
   return (
     <div className="App">
-    
       <Router>
         <Header />
         <Switch>
           <Route exact path="/">
             <Login />
           </Route>
-          <Route exact path="/home">
+          <Route path="/home">
             <Home />
+          </Route>
+          <Route path="/detail/:id">
+            <Detail />
           </Route>
         </Switch>
       </Router>
